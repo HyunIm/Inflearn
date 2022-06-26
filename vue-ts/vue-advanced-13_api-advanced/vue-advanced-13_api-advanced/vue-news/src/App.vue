ac463056b84a85ba6aeffd8bf6b475ad9086a9bd
Lim Hyun <hyunzion@gmail.com>
2022-06-26 16:43:31 +0900

    프로젝트에 타입스크립트 플러그인 추가

--- vue-ts/vue-advanced-13_api-advanced/vue-advanced-13_api-advanced/vue-news/src/App.vue
+++ vue-ts/vue-advanced-13_api-advanced/vue-advanced-13_api-advanced/vue-news/src/App.vue
@@ -1,66 +1,31 @@
 <template>
   <div id="app">
-    <spinner :loading="loading"></spinner>
-    <tool-bar></tool-bar>
-    <transition name="routing-fade" mode="out-in">
-      <router-view></router-view>
-    </transition>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import ToolBar from './components/ToolBar.vue';
-import Spinner from './components/Spinner.vue';
-import bus from './utils/bus.js';
-
-export default {
-  components: {
-    ToolBar,
-    Spinner,
-  },
-  data() {
-    return {
-      loading: false,
-    }
-  },
-  methods: {
-    onProgress() {
-      this.loading = true;
-    },
-    offProgress() {
-      this.loading = false;
-    }
-  },
-  created() {
-    bus.$on('on:progress', this.onProgress);
-    bus.$on('off:progress', this.offProgress);
-  }
-}
-</script>
-
 <style>
-body {
-  margin: 0;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
 }
 
-a {
-  color: #34495e;
-  text-decoration: none;
-}
-a:hover {
-  color: #42b883;
-  text-decoration: underline;
-}
-a.router-link-active {
-  text-decoration: underline;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-/* Router Transition */
-.routing-fade-enter-active, .routing-fade-leave-active {
-  transition: opacity .3s ease;
-}
-.routing-fade-enter, .routing-fade-leave-to
-/* .routing-fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
