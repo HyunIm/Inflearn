c05c56cdabf8c64870eb4d7315f5c32d8a96c868
Lim Hyun <hyunzion@gmail.com>
2024-03-16 20:33:16 +0900

    Spring 버전 다운그레이드
    강의 버전에 맞는 버전으로 다운그레이드

--- 예제로-배우는-핵심-스프링-배치/SpringBatchTutorial/src/main/java/com/example/SpringBatchTutorial/job/HelloWorldJobConfig.java
+++ 예제로-배우는-핵심-스프링-배치/SpringBatchTutorial/src/main/java/com/example/SpringBatchTutorial/job/HelloWorldJobConfig.java
@@ -4,36 +4,45 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
 
+/**
+ * desc: tasklet을 활용하여 Hello World를 출력
+ * run: --job.name=helloWorldJob
+ */
 @Configuration
 @RequiredArgsConstructor
 public class HelloWorldJobConfig {
 
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
     @Bean
-    public Job helloWorldJob(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
-        return new JobBuilder("helloWorldJob", jobRepository)
+    public Job helloWorldJob() {
+        return jobBuilderFactory.get("helloWorldJob")
                 .incrementer(new RunIdIncrementer())
-                .start(helloWorldStep(transactionManager, jobRepository))
+                .start(helloWorldStep())
                 .build();
     }
 
     @JobScope
     @Bean
-    public Step helloWorldStep(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
-        return new StepBuilder("helloWorldStep", jobRepository)
-                .tasklet(helloWorldTasklet(), transactionManager)
+    public Step helloWorldStep() {
+        return stepBuilderFactory.get("helloWorldStep")
+                .tasklet(helloWorldTasklet())
                 .build();
     }
 
