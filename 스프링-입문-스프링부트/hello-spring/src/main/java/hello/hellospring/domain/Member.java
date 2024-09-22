76f5ad3190d5c81e4884368378bad521a6222bb5
Lim Hyun <hyunzion@gmail.com>
2024-09-22 19:41:33 +0900

    feat: JPA

--- 스프링-입문-스프링부트/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ 스프링-입문-스프링부트/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
 
     public Long getId() {
